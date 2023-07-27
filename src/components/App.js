8183c81dbb4e95536874d6a3b930f38672ee3b5a
collins <kipkorirc583@gmail.com>
2023-07-27 12:30:32 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
 import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+import user from "./user";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
+      <Home
+        color={user.color}
+        username={user.name}
+        city={user.city}
+      />
+      <About
+        bio={user.bio}
+        links={user.links}
+      />
       <ProjectList projects={user.projects} />
     </div>
   );
